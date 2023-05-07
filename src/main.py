c927993a9a6e772b78be6242c87174d9f91f4f2d
NCF94 <ncfward@gmail.com>
2023-05-07 10:04:37 +1000

    create menu

---
+++ src/main.py
@@ -0,0 +1,11 @@
+def menu():
+    print("1. Enter 1 to view this weeks welness goals")
+    print("2. Enter 2 to mark a goal as complete")
+    print("3. Enter 3 to add your own weekly goal")
+    print("4. Enter 4 to remove a weekly goal")
+    print("5. Enter 5 to see your weekly score")
+    print("6. Enter 6 to exit")
+    choice = input("Enter your choice: ")
+    return choice
+
+user_choice = ""
