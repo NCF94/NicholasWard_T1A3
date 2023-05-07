4e4c36c32c588890953f307ffaa8647345112c2f
NCF94 <ncfward@gmail.com>
2023-05-07 13:10:52 +1000

    added body weight functions

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-from wellness_functions import add_goals, remove_goals, view_goals, mark_goals, weekly_score
+from wellness_functions import add_goals, remove_goals, view_goals, mark_goals, weekly_score, body_weight, view_weight
 
 print("Welcome to the Wellness App")
 
 file_name = "goals.csv"
+weight_file = "weight.csv"
 
 def menu():
     print("1. Enter 1 to view this weeks welness goals")
@@ -10,13 +11,15 @@ def menu():
     print("3. Enter 3 to add your own weekly goal")
     print("4. Enter 4 to remove a weekly goal")
     print("5. Enter 5 to see your weekly score")
-    print("6. Enter 6 to exit")
+    print("6. Enter 6 to input this weeks bodyweight")
+    print("7. Enter 7 to view previous weeks bodyweight")
+    print("8. Enter 8 to exit")
     choice = input("Enter your choice: ")
     return choice
 
 user_choice = ""
 
-while user_choice != "6":
+while user_choice != "8":
     user_choice = menu()
     
     if(user_choice == "1"):
@@ -34,7 +37,13 @@ while user_choice != "6":
     elif(user_choice == "5"):
         weekly_score(file_name)
         
-    elif(user_choice = "6"):
+    elif(user_choice == "6"):
+        body_weight(weight_file)
+        
+    elif(user_choice == "7"):
+        view_weight(weight_file)
+        
+    elif(user_choice == "8"):
         continue
     
     input("Press enter to continue.")
