52e50cc4cadd34756deec11ac7a147b024de2db7
NCF94 <ncfward@gmail.com>
2023-05-08 16:16:35 +1000

    create weigh punctions file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from colored import fg, bg, attr
 
-from wellness_functions import add_goals, remove_goals, view_goals, mark_goals, weekly_score, add_body_weight, view_weight, mark_weight
+from wellness_functions import add_goals, remove_goals, view_goals, mark_goals, weekly_score
+
+from weight_functions import add_body_weight, view_weight, mark_weight, remove_weight
 
 print(f"{fg('229')}Welcome to the Wellness App!{attr('reset')}")
 
@@ -32,13 +34,14 @@ def menu():
     print(f"{fg('blue')}6. Enter 6 to input this weeks bodyweight{attr('reset')}")
     print(f"{fg('blue')}7. Enter 7 to view previous weeks bodyweight{attr('reset')}")
     print(f"{fg('blue')}8. Enter 8 to mark last weeks weight if it was lower than the previous weeks.{attr('reset')}")
-    print(f"{fg('red')}9. Enter 9 to exit{attr('reset')}")
+    print(f"{fg('blue')}9. Enter 9 to remove a bodyweight{attr('reset')}")
+    print(f"{fg('red')}10. Enter 10 to exit{attr('reset')}")
     choice = input(f"Enter your choice: ")
     return choice
 
 user_choice = ""
 
-while user_choice != "9":
+while user_choice != "10":
     user_choice = menu()
     
     if(user_choice == "1"):
@@ -66,6 +69,9 @@ while user_choice != "9":
         mark_weight(weight_file_name)
         
     elif(user_choice == "9"):
+        remove_weight(weight_file_name)
+        
+    elif(user_choice == "10"):
         continue
     
     input(f"Press enter to continue.")
---
+++ src/weight_functions.py
@@ -0,0 +1,56 @@
+import csv
+
+from colored import fg, bg, attr
+
+
+def add_body_weight(weight_file_name):
+    view_weight(weight_file_name)
+    body_weight = input(f"Enter the week number and your body weight (e.g Week 1 - 80kg): ")
+    with open(weight_file_name, "a") as weight_file:
+        writer = csv.writer(weight_file)
+        writer.writerow([body_weight, "False"])
+
+
+def mark_weight(weight_file_name):
+    view_weight(weight_file_name)
+    week_weight = input(f"Enter the week/weight you want to mark as lower than the previous week: ")
+    weight_lists = []
+    with open(weight_file_name, "r") as weight_file:
+        reader = csv.reader(weight_file)
+        for row in reader:
+            if (week_weight == row[0]):
+                weight_lists.append([week_weight, "True"])
+            else:
+                weight_lists.append(row)
+    print(weight_lists)
+    with open(weight_file_name, "w") as weight_file:
+        writer = csv.writer(weight_file)
+        writer.writerows(weight_lists)
+
+
+def view_weight(weight_file_name):
+    with open(weight_file_name, "r") as weight_file:
+        reader = csv.reader(weight_file)
+        reader.__next__()
+        for row in reader:
+            if (row[1] == "True"):
+                print(
+                    f"{fg('blue')}Bodyweight:{attr('reset')} '{row[0]}' {fg('green')}Congratulations, this weeks weight is less then last week! Keep going, you got this!{attr('reset')}")
+            else:
+                print(
+                    f"{fg('blue')}Bodyweight:{attr('reset')} '{row[0]}' {fg('red')}This weeks weight is more then last weeks. Thats okay though, this is a long process{attr('reset')}")
+
+
+def remove_weight(weight_file_name):
+    view_weight(weight_file_name)
+    week_weight = input(f"Enter the name of the goal you wish to remove: ")
+    weight_lists = []
+    with open(weight_file_name, "r") as weight_file:
+        reader = csv.reader(weight_file)
+        for row in reader:
+            if (week_weight != row[0]):
+                weight_lists.append(row)
+    print(weight_lists)
+    with open(weight_file_name, "w") as weight_file:
+        writer = csv.writer(weight_file)
+        writer.writerows(weight_lists)
